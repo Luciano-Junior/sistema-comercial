6a85f42f71bf48d32a6fccfdf00d502e5ec8aeb7
Luciano-Junior <tec.luciano.ti@gmail.com>
2025-02-03 18:56:08 -0300

    Remove produto/config

--- src/main/java/com/c9tecnologia/comercial/config/ModelMapperConfig.java
+++ src/main/java/com/c9tecnologia/comercial/config/ModelMapperConfig.java
@@ -1,4 +1,4 @@
-package com.c9tecnologia.comercial.produto.config;
+package com.c9tecnologia.comercial.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
